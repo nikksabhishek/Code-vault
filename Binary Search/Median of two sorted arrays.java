0eea6d45fb8023142623685f5e0b97ccc670e319
Abhishek Yadav <58683166+nikksabhishek@users.noreply.github.com>
2020-02-14 15:46:33 +0530

    Update Median of two sorted arrays.java

--- Binary Search/Median of two sorted arrays.java
+++ Binary Search/Median of two sorted arrays.java
@@ -29,8 +29,8 @@ public class MedianOfTwoSortedArrayOfDifferentLength {
             int partitionX = (low + high)/2;
             int partitionY = (x + y + 1)/2 - partitionX;
 
-            //if partitionX is 0 it means nothing is there on left side. Use -INF for maxLeftX
-            //if partitionX is length of input then there is nothing on right side. Use +INF for minRightX
+            //if partitionX is 0, left side of the array is empty. Use -INF for maxLeftX
+            //if partitionX is length of input, right side of the array is empty. Use +INF for minRightX
             int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : input1[partitionX - 1];
             int minRightX = (partitionX == x) ? Integer.MAX_VALUE : input1[partitionX];
 
@@ -38,7 +38,7 @@ public class MedianOfTwoSortedArrayOfDifferentLength {
             int minRightY = (partitionY == y) ? Integer.MAX_VALUE : input2[partitionY];
 
             if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-                //We have partitioned array at correct place
+                // partitioned array at correct place
                 // Now get max of left elements and min of right elements to get the median in case of even length combined array size
                 // or get max of left for odd length combined array size.
                 if ((x + y) % 2 == 0) {
@@ -48,12 +48,12 @@ public class MedianOfTwoSortedArrayOfDifferentLength {
                 }
             } else if (maxLeftX > minRightY) { //we are too far on right side for partitionX. Go on left side.
                 high = partitionX - 1;
-            } else { //we are too far on left side for partitionX. Go on right side.
+            } else { //too far on left side for partitionX. Go on right side.
                 low = partitionX + 1;
             }
         }
 
-        //Only we we can come here is if input arrays were not sorted. Throw in that scenario.
+        //if input arrays were not sorted. Throw in that scenario.
         throw new IllegalArgumentException();
     }
 
